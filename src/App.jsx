87e172f568dd3f0b389281c51a819e6caa96a93b
VivekRajput001 <vivekrajput4035200@gmail.com>
2025-08-07 16:36:40 +0530

    Moved project files to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
-      <div className="text-red-500 text-md bg-green-300 p-4">
-        Home
-      </div>
-
-
+    <Home />
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <div class="h-[80px] flex items-center max-md:h-[70px]">
+                <img class="pl-6 mr-3 h-14 w-auto max-md:h-11" src="./images/jec logo QQ.webp" alt="">
+                    <div class="collegeName">
+                        <p class="text-2xl font-semibold max-md:text-xl max-md:mb-[-8px]">Robotics Laboratory</p>
+                        <a class="no-underline text-black text-sm max-md:text-xs" target="_blank" href="https://www.jecjabalpur.ac.in/">Jabalpur
+                            Engineering College</a>
+                    </div>
+            </div>
+
+            <nav class="nav w-full h-14 bg-[#1e2a78]">
+                <div class="flex justify-around h-full items-center max-w-[90%] m-auto">
+                    <a class="text-white text-base active:transition-transform active:duration-80 active:ease-in-out active:scale-107 hover:transition-transform hover:duration-80 hover:ease-in-out hover:scale-107"
+                        href="">Home</a>
+                    <a class="text-white text-base active:transition-transform active:duration-80 active:ease-in-out active:scale-107 hover:transition-transform hover:duration-80 hover:ease-in-out hover:scale-107"
+                        href="">Updates</a>
+                    <a class="text-white text-base active:transition-transform active:duration-80 active:ease-in-out active:scale-107 hover:transition-transform hover:duration-80 hover:ease-in-out hover:scale-107"
+                        href="">Projects</a>
+                    <a class="text-white text-base active:transition-transform active:duration-80 active:ease-in-out active:scale-107 hover:transition-transform hover:duration-80 hover:ease-in-out hover:scale-107"
+                        href="">Equipments</a>
+                    <a class="text-white text-base active:transition-transform active:duration-80 active:ease-in-out active:scale-107 hover:transition-transform hover:duration-80 hover:ease-in-out hover:scale-107 max-sm:hidden"
+                        href="">Institute</a>
+                    <a class="text-white text-base active:transition-transform active:duration-80 active:ease-in-out active:scale-107 hover:transition-transform hover:duration-80 hover:ease-in-out hover:scale-107 max-sm:hidden"
+                        href="">Contact Us</a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
